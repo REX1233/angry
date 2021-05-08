d45c3fd9b632a81b57963ce56f39a92b746eb248
REX1233 <divya76226699@gmail.com>
2021-05-07 22:00:03 -0700

    you have added platform

--- sketch.js
+++ sketch.js
@@ -22,7 +22,8 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+    platform= new Ground(150,300,300,170)
+   
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -59,4 +60,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+platform.display()
 }
